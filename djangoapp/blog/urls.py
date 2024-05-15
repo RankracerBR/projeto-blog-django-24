efece4b667cd97748800f87c02e0a9bcdb92e1b0
Augusto Pontes <augustopontes010@gmail.com>
2024-05-15 02:58:11 -0300

    Alteração no design e adição do django summernote para auxiliar na criação dos posts no blog

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import index
+from blog.views import index, page, post
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('post/<slug:slug>/', post, name='post'),
+    path('page/', page, name='page'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,38 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 
+PER_PAGE = 9
+
 def index(request):
-    return render(
-        request, 'blog/pages/index.html',
-        {
-            'nome': 'Augusto Pontes'
-        }
+    posts = (
+        Post
+        .objects
+        .filter(is_published=True)
+        .order_by('-pk')
         )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number) 
+    return render(
+        request, 
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def page(request):
+    return render(
+        request, 
+        'blog/pages/index.html'
+    )
+
+def post(request):
+    return render(
+        request, 
+        'blog/pages/index.html'
+    )
